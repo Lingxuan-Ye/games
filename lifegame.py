717899757b26e450ed590b80e3f27753ae4c7863
Lingxuan Ye <lingxuan.ye@icloud.com>
2023-02-11 20:07:12 -0500

    Executeble

---
+++ lifegame.py
@@ -0,0 +1,41 @@
+import numpy as np
+from time import sleep
+import os
+
+shape = (40, 40)
+frame = np.random.randint(0, 2, (shape))
+cache = np.array(frame)
+
+
+def fprint(frame):
+    for i in frame:
+        print(''.join('⬜' if j == 0 else '⬛' for j in i))
+
+
+def fgen():
+    global frame
+    loc = np.array([0, 0], dtype=np.int8)
+    for index, status in np.ndenumerate(cache):
+        count = 0
+        for rloc in ((x, y) for x in (-1, 0, 1) for y in (-1, 0, 1)):
+            if rloc == (0, 0):
+                continue
+            loc[0], loc[1] = index[0] + rloc[0], index[1] + rloc[1]
+            if loc[0] == cache.shape[0]:
+                loc[0] = 0
+            if loc[1] == cache.shape[1]:
+                loc[1] = 0
+            if cache[loc[0], loc[1]] == 1:
+                count += 1
+        if status == 0 and count == 3:
+            frame[index[0], index[1]] = 1
+        elif status == 1 and count not in {2, 3}:
+            frame[index[0], index[1]] = 0
+
+
+while True:
+    fprint(frame)
+    fgen()
+    cache[:] = frame
+    sleep(0.01)
+    os.system('clear')
