531ac2f1f2d730e7b24d4d06a052eb2ccc02c890
Lingxuan Ye <lingxuan.ye@icloud.com>
2023-02-14 04:31:14 -0500

    Last version with no `asyncio`

--- lifegame.py
+++ lifegame.py
@@ -4,7 +4,7 @@ Lifegame
 Use bit to represent status may be more efficient but, nah.
 """
 import argparse
-import os
+import subprocess
 from random import choice
 from time import sleep
 from typing import Literal, Sequence
@@ -33,24 +33,33 @@ class LifeGame:
     }
 
     shape: tuple[int, int]
-    fps: float = 60.0
+    fps: int | float
 
-    __symbols: tuple[str, str]
     __frame: np.ndarray
     __prev: np.ndarray
     __loc: np.ndarray
-    __cache: list[str] = []
+    __symbols: tuple[str, str]
+    __to_print: list[str]
+
+    __margin: int
+    __margin_top: str
+    __margin_left: str
 
     def __init__(
         self,
-        shape: Sequence[int],
-        symbol_type: Literal['alpha', 'block', 'digit', 'emoji']
+        shape: Sequence[int] = (32, 32),
+        symbol_type: Literal['alpha', 'block', 'digit', 'emoji'] = 'block',
+        margin: int = 2,
+        fps: int | float = 60
     ) -> None:
         self.shape = (shape[0], shape[1])
+        self.set_symbols(symbol_type)
+        self.margin = margin
+        self.fps = fps
         self.__frame = np.random.choice((0, 1), self.shape)
         self.__prev = self.__frame.copy()
         self.__loc = np.empty(2, dtype=np.int8)
-        self.set_symbols(symbol_type)
+        self.__to_print = []
 
     @property
     def symbols(self) -> tuple[str, str]:
@@ -66,18 +75,29 @@ class LifeGame:
             choice(self.SYMBOLS['alive'][type_])
         )
 
+    @property
+    def margin(self) -> int:
+        return self.__margin
+
+    @margin.setter
+    def margin(self, __value: int) -> None:
+        self.__margin_top = '\n' * __value
+        self.__margin_left = ' ' * __value
+        self.__margin = __value
+
     def print(self, *arg, **kwargs) -> None:
-        self.__cache.clear()
+        self.__to_print.clear()
         for row in self.__frame:
-            self.__cache.append(''.join(self.__symbols[col] for col in row))
-        print('\n'.join(self.__cache), *arg, **kwargs)
+            self.__to_print.append(
+                self.__margin_left + ''.join(self.__symbols[i] for i in row)
+            )
+        print(self.__margin_top + '\n'.join(self.__to_print), *arg, **kwargs)
 
     def generate(self) -> None:
         for index, is_alive in np.ndenumerate(self.__prev):
             count = 0
             for rloc in self.RLOCS:
-                for i in range(2):
-                    self.__loc[i] = (index[i] + rloc[i]) % self.__prev.shape[i]
+                self.__loc[:] = (index + rloc) % self.shape
                 if self.__prev[*self.__loc]:
                     count += 1
             if (not is_alive) and (count == 3):
@@ -89,19 +109,21 @@ class LifeGame:
     def run(self) -> None:
         frame_duration = 1 / self.fps
         while True:
-            os.system('clear')
+            subprocess.run('clear || cls', shell=True)
             self.print()
             self.generate()
             sleep(frame_duration)
 
 
 def main() -> None:
+    
     parser = argparse.ArgumentParser('Life Game')
 
     parser.add_argument(
         '--nrows',
         default=32,
         type=int,
+        help='number of rows',
         metavar=''
     )
 
@@ -109,23 +131,42 @@ def main() -> None:
         '--ncols',
         default=32,
         type=int,
+        help='number of columns',
         metavar=''
     )
-
-    parser.add_argument('--fps', default=60, type=int, metavar='')
 
     parser.add_argument(
         '--symbols',
         default='block',
         choices=('alpha', 'block', 'digit', 'emoji'),
+        help='symbols to represent status (dead / alive)',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--fps',
+        default=60,
+        type=int,
+        help='frames per second',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--margin',
+        default=2,
+        type=int,
+        help='margin width',
         metavar=''
     )
 
     args = parser.parse_args()
-    
-    lifegame = LifeGame((args.nrows, args.ncols), args.symbols)
-    lifegame.fps = args.fps
-    lifegame.run()
+
+    LifeGame(
+        shape=(args.nrows, args.ncols),
+        symbol_type=args.symbols,
+        margin=args.margin,
+        fps=args.fps
+    ).run()
 
 
 if __name__ == '__main__':
