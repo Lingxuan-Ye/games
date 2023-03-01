01d921932a603decbbc438caae727b67efc5a059
Lingxuan Ye <lingxuan.ye@icloud.com>
2023-03-01 07:29:04 -0500

    Refactor `LifeGame` for readability

--- lifegame.py
+++ lifegame.py
@@ -1,108 +1,91 @@
-"""
-Lifegame
-
-Use bit to represent status may be more efficient but, nah.
-"""
 import argparse
 import asyncio
 import subprocess
 from random import choice
-from typing import Literal
+from textwrap import indent
+from typing import Literal, NamedTuple, Sequence
 
 import numpy as np
+from numpy.typing import NDArray
+
+try:
+    from .const import SYMBOLS
+    from .models.frame import RevertibleFrame
+except ImportError:
+    from const import SYMBOLS
+    from models.frame import RevertibleFrame
+
+
+class Cell:
+
+    dead: str
+    alive: str
+
+    def __init__(
+        self,
+        type: Literal['alpha', 'binary', 'block', 'emoji', 'legacy'] = 'block'
+    ) -> None:
+        for k, v in SYMBOLS[type.strip().lower()].items():
+            if isinstance(v, list):
+                v = choice(v)
+            if k == 'dead':
+                self.dead = v
+            elif k == 'alive':
+                self.alive = v
+
+
+class Padding(NamedTuple):
+
+    top: str
+    left: str
 
 
 class LifeGame:
 
-    SYMBOLS = {
-        'dead': {
-            'alpha': 'X',
-            'block': '⬜',
-            'digit': '0',
-            'emoji': '😅😇🤪😴🤢🤮🥶😰😭'
-        },
-        'alive': {
-            'alpha': 'O',
-            'block': '⬛',
-            'digit': '1',
-            'emoji': '🤣🥰😘😋🤗🤤🥵🥳😤'
-        }
-    }
-    RLOCS = np.array([(-1, 0, 1)] * 2, dtype=np.int8)
+    RESET_CURSOR = '\033[1;1H'
 
-    shape: np.ndarray
-    frames_per_second: int | float
+    frame_duration: float
 
-    __symbols: tuple[str, str]
-    __row_offset: int
-    __top_padding: str
-    __col_offset: int
-    __left_padding: str
-
-    __next_frame: np.ndarray
-    __current_frame: np.ndarray
-    __to_print: list[str]
-    __locs: np.ndarray
+    __cell: Cell
+    __frame: RevertibleFrame
+    __padding: Padding
+    __locs: tuple[NDArray, ...]
+    __rlocs: tuple[NDArray, ...]
+    __repr_cache: NDArray
+    __print_queue: asyncio.Queue[str]
 
     def __init__(
         self,
         nrows: int = 32,
         ncols: int = 32,
-        frames_per_second: int | float = 10,
-        symbol_type: Literal['alpha', 'block', 'digit', 'emoji'] = 'block',
+        *,
+        cell: Literal['alpha', 'binary', 'block', 'emoji', 'legacy'] = 'block',
+        fps: int | float = 24,
         row_offset: int = 1,
         col_offset: int = 2,
         seed: int | None = None
     ) -> None:
-
-        self.shape = np.array([nrows, ncols])
-        self.frames_per_second = frames_per_second
-        self.row_offset = row_offset
-        self.col_offset = col_offset
+        self.__cell = Cell(cell)
+        self.__frame = RevertibleFrame((nrows, ncols), np.bool_)
+        shape = self.__frame.shape
+        self.__frame.prev[:] = np.random.randint(0, 2, shape, np.bool_)
+        self.frame_duration = 1 / fps
+        self.__padding = Padding('\n' * row_offset, ' ' * col_offset)
         if seed is not None:
             self.seed = seed
-        self.set_symbols(symbol_type)
-
-        init_frame = np.random.randint(0, 2, self.shape, dtype=np.uint8)
-        self.__current_frame = init_frame
-        self.__next_frame = init_frame.copy()
-        self.__to_print = []
-        self.__locs = np.empty(
-            shape=(2, 3),
-            dtype=np.min_scalar_type(np.max(self.shape))
-        )
+        loc_dtype = np.min_scalar_type(np.max(shape))
+        self.__locs = np.ix_(np.empty(3, loc_dtype), np.empty(3, loc_dtype))
+        self.__rlocs = np.ix_([-1, 0, 1], [-1, 0, 1])
+        self.__repr_cache = np.empty(shape, dtype='<U2')
+        self.__print_queue = asyncio.Queue()
 
     @property
-    def symbols(self) -> tuple[str, str]:
-        return self.__symbols
-
-    def set_symbols(
-        self,
-        type_: Literal['alpha', 'block', 'digit', 'emoji']
-    ) -> None:
-        type_ = type_.strip().lower()  # type: ignore
-        self.__symbols = (
-            choice(self.SYMBOLS['dead'][type_]),
-            choice(self.SYMBOLS['alive'][type_])
-        )
+    def cell(self) -> Cell:
+        return self.__cell
 
     @property
-    def row_offset(self) -> int:
-        return self.__row_offset
-
-    @row_offset.setter
-    def row_offset(self, __value: int) -> None:
-        self.__top_padding = '\n' * __value
-        self.__row_offset = __value
-
-    @property
-    def col_offset(self) -> int:
-        return self.__col_offset
-
-    @col_offset.setter
-    def col_offset(self, __value: int) -> None:
-        self.__left_padding = ' ' * __value
-        self.__col_offset = __value
+    def frame(self) -> RevertibleFrame:
+        self.__frame
 
     @property
     def seed(self) -> int:
@@ -112,118 +95,88 @@ class LifeGame:
     def seed(self, __value: int) -> None:
         np.random.seed(__value)
 
-    async def print(self, *args, **kwargs) -> None:
-        self.__to_print.clear()
-        for row in self.__current_frame:
-            self.__to_print.append(
-                self.__left_padding + ''.join(self.__symbols[i] for i in row)
-            )
-        message = self.__top_padding + '\n'.join(self.__to_print)
-        print(f'\033[1;1H{message}', *args, **kwargs)
+    async def sleep(self) -> None:
+        await asyncio.sleep(self.frame_duration)
 
-    async def generate(self) -> None:
-        for index, is_alive in np.ndenumerate(self.__current_frame):
-            self.__locs[:] = (
-                np.array(list(index)).reshape(2, 1) + self.RLOCS
-            ) % self.shape.reshape(2, 1)  # mod in case index out of range
-            count = self.__current_frame[np.ix_(*self.__locs)].sum() - is_alive
+    def generate(self) -> None:
+        current, next = self.__frame.prev, self.__frame.frame
+        shape = self.__frame.shape
+        for index, is_alive in np.ndenumerate(current):
+            self.__locs[0][:] = (index[0] + self.__rlocs[0]) % shape[0]
+            self.__locs[1][:] = (index[1] + self.__rlocs[1]) % shape[1]
+            count = current[self.__locs].sum() - is_alive
             if (not is_alive) and (count == 3):
-                self.__next_frame[index] = 1
+                next[index] = 1
             elif is_alive and (count not in {2, 3}):
-                self.__next_frame[index] = 0
+                next[index] = 0
 
-    async def __run(self) -> None:
-        frame_duration = 1 / self.frames_per_second
+    async def repr(self) -> None:
+        self.generate()
+        current = self.__frame.prev
+        self.__repr_cache[current] = self.cell.alive
+        self.__repr_cache[~current] = self.cell.dead
+        self.__frame.record()
+        await self.__print_queue.put(
+            '\n'.join(''.join(row) for row in self.__repr_cache)
+        )
+
+    async def print(self) -> None:
+        repr_ = await self.__print_queue.get()
+        padded = self.__padding.top + indent(repr_, self.__padding.left)
+        print(self.RESET_CURSOR + padded)
+
+    async def _run(self) -> None:
         subprocess.run('clear || cls', shell=True)
         while True:
-            await asyncio.gather(
-                asyncio.sleep(frame_duration),
-                self.print(),
-                self.generate()
-            )
-            self.__current_frame[:] = self.__next_frame
+            await asyncio.gather(self.sleep(), self.repr(), self.print())
 
     def run(self) -> None:
-        asyncio.run(self.__run())
+        asyncio.run(self._run())
 
-    @classmethod
-    def run_from_command_line(cls) -> None:
 
-        HELP = {
-            'nrows': 'number of rows',
-            'ncols': 'number of columns',
-            'fps': 'frames per second',
-            'symbol-type': 'symbols to represent status (dead / alive)',
-            'row-offset': 'margin width to the top',
-            'col-offset': 'margin width to the left',
-            'seed': 'set seed (this does not affect emoji selection)'
+def main(args: Sequence | None = None) -> None:
+
+    HELP = {
+        'nrows': 'number of rows',
+        'ncols': 'number of columns',
+        'cell': 'specify symbol pair to represent cell status (dead / alive)',
+        'fps': 'frames per second',
+        'row-offset': 'margin width to the top',
+        'col-offset': 'margin width to the left',
+        'seed': 'set seed (this does not affect emoji selection)'
+    }
+
+    parser = argparse.ArgumentParser('Life Game')
+
+    parser.add_argument('-r', '--nrows', type=int, help=HELP['nrows'])
+
+    parser.add_argument('-c', '--ncols', type=int, help=HELP['ncols'])
+
+    parser.add_argument(
+        '--cell',
+        choices=('alpha', 'binary', 'block', 'emoji', 'legacy'),
+        help=HELP['cell']
+    )
+
+    parser.add_argument('--fps', type=float, help=HELP['fps'])
+
+    parser.add_argument(
+        '-y', '--row-offset', type=int, help=HELP['row-offset']
+    )
+
+    parser.add_argument(
+        '-x', '--col-offset', type=int, help=HELP['col-offset']
+    )
+
+    parser.add_argument('-s', '--seed', type=int, help=HELP['seed'])
+
+    LifeGame(
+        **{
+            option: value
+            for option, value in parser.parse_args(args)._get_kwargs()
+            if value is not None
         }
-
-        parser = argparse.ArgumentParser('Life Game')
-
-        parser.add_argument(
-            '-r',
-            '--nrows',
-            type=int,
-            help=HELP['nrows'],
-            metavar=''
-        )
-
-        parser.add_argument(
-            '-c',
-            '--ncols',
-            type=int,
-            help=HELP['ncols'],
-            metavar=''
-        )
-
-        parser.add_argument(
-            '-f',
-            '--fps',
-            type=float,
-            help=HELP['fps'],
-            metavar='',
-            dest='frames_per_second'
-        )
-
-        parser.add_argument(
-            '-s',
-            '--symbol-type',
-            choices=('alpha', 'block', 'digit', 'emoji'),
-            help=HELP['symbol-type'],
-            metavar=''
-        )
-
-        parser.add_argument(
-            '-R',
-            '--row-offset',
-            type=int,
-            help=HELP['row-offset'],
-            metavar=''
-        )
-
-        parser.add_argument(
-            '-C',
-            '--col-offset',
-            type=int,
-            help=HELP['col-offset'],
-            metavar=''
-        )
-
-        parser.add_argument(
-            '-S',
-            '--seed',
-            type=int,
-            help=HELP['seed'],
-            metavar=''
-        )
-
-        cls(**{
-            param: arg
-            for param, arg in parser.parse_args()._get_kwargs()
-            if arg is not None
-        }).run()
-
+    ).run()
 
 if __name__ == '__main__':
-    LifeGame.run_from_command_line()
+    main()
--- models/frame.py
+++ models/frame.py
@@ -1,7 +1,9 @@
 import abc
-from typing import Any, Iterable, MutableSequence, Self, Sequence, final
+from types import EllipsisType
+from typing import Any, Generic, Iterable, MutableSequence, Self, Sequence, TypeVar, final
 
 import numpy as np
+from numpy.typing import NDArray
 
 
 class Shape(tuple[int]):
@@ -21,40 +23,42 @@ class Shape(tuple[int]):
 
 
 # None for `np.newaxis`
-_0DIndex = MutableSequence | np.ndarray | int | slice | ellipsis | None
+_0DIndex = MutableSequence | NDArray | int | slice | EllipsisType | None
 _NDIndex = tuple[_0DIndex | tuple, ...]
 _Index = _0DIndex | _NDIndex
-_Value = np.number | np.ndarray
+_Value = np.number | NDArray
+
+T = TypeVar('T', bound=np.number)
 
 
-class _BaseFrame(abc.ABC):
+class _BaseFrame(abc.ABC, Generic[T]):
 
-    dtype: type[np.number]
+    dtype: type[T]
 
-    __shape: Shape
-    __frame: np.ndarray
+    _shape: Shape
+    _frame: NDArray[T]
 
     def __init__(
         self,
         shape: Iterable[int],
-        dtype: type[np.number] | None = None
+        dtype: type[T] | None = None
     ) -> None:
-        self.__shape = Shape(shape)
+        self._shape = Shape(shape)
         if dtype is not None:
             self.dtype = dtype
         self.init_frame()
 
     @property
     def shape(self) -> Shape:
-        return self.__shape
+        return self._shape
 
     @property
     def ndim(self) -> int:
-        return len(self.__shape)
+        return len(self._shape)
 
     @property
-    def frame(self) -> np.ndarray:
-        return self.__frame
+    def frame(self) -> NDArray[T]:
+        return self._frame
 
     @abc.abstractmethod
     def __getitem__(self, index: _Index, /) -> Any:
@@ -66,8 +70,8 @@ class _BaseFrame(abc.ABC):
 
     @final
     def resize(self, shape: Iterable[int]) -> Self:
-        self.__shape = Shape(shape)
-        self.__frame = np.resize(self.__frame, self.__shape)
+        self._shape = Shape(shape)
+        self._frame = np.resize(self._frame, self._shape)
         return self
 
     @abc.abstractmethod
@@ -75,12 +79,12 @@ class _BaseFrame(abc.ABC):
         ...
 
     @final
-    def set_frame(self, frame: np.ndarray) -> Self:
-        if self.__shape == frame.shape:
-            self.__frame[:] = frame
+    def set_frame(self, frame: NDArray[T]) -> Self:
+        if self._shape == frame.shape:
+            self._frame[:] = frame
             return self
-        self.__shape = Shape(frame.shape)
-        self.__frame = frame.copy()
+        self._shape = Shape(frame.shape)
+        self._frame = frame.copy()
         return self
 
 
@@ -88,11 +92,11 @@ class _SubcriptableFrame(_BaseFrame):
 
     @final
     def __getitem__(self, index: _Index, /) -> _Value:
-        return self.__frame[self.mod_index(index)]
+        return self._frame[self.mod_index(index)]
 
     @final
     def __setitem__(self, index: _Index, value: _Value, /) -> None:
-        self.__frame[self.mod_index(index)] = value
+        self._frame[self.mod_index(index)] = value
 
     @final
     def mod_index(self, index: _Index) -> _Index:
@@ -102,15 +106,15 @@ class _SubcriptableFrame(_BaseFrame):
 
     @final
     def __mod_index(self, index: _Index, axis: int) -> _Index:
-        if isinstance(index, MutableSequence | np.ndarray):
-            return np.asarray(index) % self.__shape[axis]
+        if isinstance(index, MutableSequence | NDArray):
+            return np.asarray(index) % self._shape[axis]
         if isinstance(index, int):
-            return index % self.__shape[axis]
+            return index % self._shape[axis]
         if isinstance(index, slice):
             return slice(
-                index.start % self.__shape[axis],
-                index.stop % self.__shape[axis],
-                index.step % self.__shape[axis]
+                index.start % self._shape[axis],
+                index.stop % self._shape[axis],
+                index.step % self._shape[axis]
             )
         return index
 
@@ -118,100 +122,103 @@ class _SubcriptableFrame(_BaseFrame):
 class Frame(_SubcriptableFrame):
 
     def init_frame(self) -> Self:
-        self.__frame = np.empty(self.__shape, self.dtype)
+        self._frame = np.empty(self._shape, self.dtype)
         return self
 
 
-class RGB(Frame):
+U = TypeVar('U', bound=np.unsignedinteger)
+
+
+class RGB(Frame, Generic[U]):
 
     dtype = np.uint8
 
     def __init__(
         self,
         shape: Iterable[int],
-        dtype: type[np.unsignedinteger] | None = None
+        dtype: type[U] | None = None
     ) -> None:
         super().__init__((*shape, 3), dtype)
 
     @property
     def ndim(self) -> int:
-        return len(self.__shape) - 1
+        return len(self._shape) - 1
 
     @property
-    def red(self) -> np.ndarray:
-        return self.__frame[..., 0]
+    def red(self) -> NDArray[U]:
+        return self._frame[..., 0]
 
     @property
-    def green(self) -> np.ndarray:
-        return self.__frame[..., 1]
+    def green(self) -> NDArray[U]:
+        return self._frame[..., 1]
 
     @property
-    def blue(self) -> np.ndarray:
-        return self.__frame[..., 2]
+    def blue(self) -> NDArray[U]:
+        return self._frame[..., 2]
 
     R, G, B = red, green, blue
 
 
-class RGBA(RGB):
+class RGBA(RGB[U]):
 
     def __init__(
         self,
         shape: Iterable[int],
-        dtype: type[np.unsignedinteger] | None = None
+        dtype: type[U] | None = None
     ) -> None:
         super(RGB, self).__init__((*shape, 4), dtype)
 
     @property
-    def alpha(self) -> np.ndarray:
-        return self.__frame[..., 3]
+    def alpha(self) -> NDArray[U]:
+        return self._frame[..., 3]
 
     A = alpha
 
 
-class RevertibleFrame(Frame):
+class RevertibleFrame(Frame, Generic[T]):
 
-    __prev: np.ndarray
+    _prev: NDArray[T]
 
     @property
-    def prev(self) -> np.ndarray:
-        return self.__prev
+    def prev(self) -> NDArray[T]:
+        return self._prev
 
     def init_frame(self) -> Self:
         super().init_frame()
-        self.__prev = self.__frame.copy()
+        self._prev = self._frame.copy()
         return self
 
     @final
     def record(self) -> Self:
-        self.__prev[:] = self.__frame
+        self._prev[:] = self._frame
         return self
 
     @final
     def revert(self) -> Self:
-        self.__frame[:] = self.__prev
+        self._frame[:] = self._prev
         return self
 
 
-class RevertibleRGB(RGB, RevertibleFrame):
+class RevertibleRGB(RGB[U], RevertibleFrame):
 
     @property
-    def prev_red(self) -> np.ndarray:
-        return self.__prev[..., 0]
+    def prev_red(self) -> NDArray[U]:
+        return self._prev[..., 0]
 
     @property
-    def prev_green(self) -> np.ndarray:
-        return self.__prev[..., 1]
+    def prev_green(self) -> NDArray[U]:
+        return self._prev[..., 1]
 
     @property
-    def prev_blue(self) -> np.ndarray:
-        return self.__prev[..., 2]
+    def prev_blue(self) -> NDArray[U]:
+        return self._prev[..., 2]
 
 
-class RevertibleRGBA(RGBA, RevertibleRGB):
+class RevertibleRGBA(RGBA[U], RevertibleRGB):
 
     @property
-    def prev_alpha(self) -> np.ndarray:
-        return self.__prev[..., 3]
+    def prev_alpha(self) -> NDArray[U]:
+        return self._prev[..., 3]
 
 
 class BitFrame(Frame):
