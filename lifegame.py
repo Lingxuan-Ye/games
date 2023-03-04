81d01f2b038795191d1fa270a7319d137203b59d
Lingxuan Ye <lingxuan.ye@icloud.com>
2023-03-04 04:56:29 -0500

    lifegame.py: Object-oriented version
    Squashed commit of the following:
    
    commit 531ac2f1f2d730e7b24d4d06a052eb2ccc02c890
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Tue Feb 14 04:31:14 2023 -0500
    
        Last version with no `asyncio`
    
    commit 0473c8d176feb9c9e60bc5861e1ec384fa311fe0
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Mon Feb 13 08:05:40 2023 -0500
    
        Format
    
    commit 41e2e1bdf34113515214872795cb9a7b6c76b527
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Mon Feb 13 07:48:55 2023 -0500
    
        Update
    
    commit 828cc15f0356080297965f262c8939c116de190d
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Mon Feb 13 04:53:01 2023 -0500
    
        Bug fix
    
    commit f2582c1050fcdb5b0dc5b57876a32b1f42c52673
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Sat Feb 11 20:37:35 2023 -0500
    
        Sort imports and misc
    
    commit 717899757b26e450ed590b80e3f27753ae4c7863
    Author: Lingxuan Ye <lingxuan.ye@icloud.com>
    Date:   Sat Feb 11 20:07:12 2023 -0500
    
        Executeble

--- lifegame.py
+++ lifegame.py
@@ -3,45 +3,171 @@ Lifegame
 
 Use bit to represent status may be more efficient but, nah.
 """
-import os
+import argparse
+import subprocess
+from random import choice
 from time import sleep
+from typing import Literal, Sequence
 
 import numpy as np
 
-shape = (40, 40)
-rlocs = [(x, y) for x in (-1, 0, 1) for y in (-1, 0, 1) if x or y]
-frame = np.random.randint(0, 2, (shape))
-cache = np.array(frame)
+
+class LifeGame:
+
+    RLOCS = np.array(
+        [[x, y] for x in (-1, 0, 1) for y in (-1, 0, 1) if x or y]
+    )
+    SYMBOLS = {
+        'dead': {
+            'alpha': 'O',
+            'block': '⬜',
+            'digit': '0',
+            'emoji': '😅😇🤪😴🤢🤮🥶😰😭'
+        },
+        'alive': {
+            'alpha': 'X',
+            'block': '⬛',
+            'digit': '1',
+            'emoji': '🤣🥰😘😋🤗🤤🥵🥳😤'
+        }
+    }
+
+    shape: tuple[int, int]
+    fps: int | float
+
+    __frame: np.ndarray
+    __prev: np.ndarray
+    __loc: np.ndarray
+    __symbols: tuple[str, str]
+    __to_print: list[str]
+
+    __margin: int
+    __margin_top: str
+    __margin_left: str
+
+    def __init__(
+        self,
+        shape: Sequence[int] = (32, 32),
+        symbol_type: Literal['alpha', 'block', 'digit', 'emoji'] = 'block',
+        margin: int = 2,
+        fps: int | float = 60
+    ) -> None:
+        self.shape = (shape[0], shape[1])
+        self.set_symbols(symbol_type)
+        self.margin = margin
+        self.fps = fps
+        self.__frame = np.random.choice((0, 1), self.shape)
+        self.__prev = self.__frame.copy()
+        self.__loc = np.empty(2, dtype=np.int8)
+        self.__to_print = []
+
+    @property
+    def symbols(self) -> tuple[str, str]:
+        return self.__symbols
+
+    def set_symbols(
+        self,
+        type_: Literal['alpha', 'block', 'digit', 'emoji']
+    ) -> None:
+        type_ = type_.strip().lower()  # type: ignore
+        self.__symbols = (
+            choice(self.SYMBOLS['dead'][type_]),
+            choice(self.SYMBOLS['alive'][type_])
+        )
+
+    @property
+    def margin(self) -> int:
+        return self.__margin
+
+    @margin.setter
+    def margin(self, __value: int) -> None:
+        self.__margin_top = '\n' * __value
+        self.__margin_left = ' ' * __value
+        self.__margin = __value
+
+    def print(self, *arg, **kwargs) -> None:
+        self.__to_print.clear()
+        for row in self.__frame:
+            self.__to_print.append(
+                self.__margin_left + ''.join(self.__symbols[i] for i in row)
+            )
+        print(self.__margin_top + '\n'.join(self.__to_print), *arg, **kwargs)
+
+    def generate(self) -> None:
+        for index, is_alive in np.ndenumerate(self.__prev):
+            count = 0
+            for rloc in self.RLOCS:
+                self.__loc[:] = (index + rloc) % self.shape
+                if self.__prev[*self.__loc]:
+                    count += 1
+            if (not is_alive) and (count == 3):
+                self.__frame[index] = 1
+            elif is_alive and (count not in {2, 3}):
+                self.__frame[index] = 0
+        self.__prev[:] = self.__frame
+
+    def run(self) -> None:
+        frame_duration = 1 / self.fps
+        while True:
+            subprocess.run('clear || cls', shell=True)
+            self.print()
+            self.generate()
+            sleep(frame_duration)
 
 
-def fprint(frame):
-    for i in frame:
-        print(''.join('⬜' if j == 0 else '⬛' for j in i))
+def main() -> None:
+
+    parser = argparse.ArgumentParser('Life Game')
+
+    parser.add_argument(
+        '--nrows',
+        default=32,
+        type=int,
+        help='number of rows',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--ncols',
+        default=32,
+        type=int,
+        help='number of columns',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--symbols',
+        default='block',
+        choices=('alpha', 'block', 'digit', 'emoji'),
+        help='symbols to represent status (dead / alive)',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--fps',
+        default=60,
+        type=int,
+        help='frames per second',
+        metavar=''
+    )
+
+    parser.add_argument(
+        '--margin',
+        default=2,
+        type=int,
+        help='margin width',
+        metavar=''
+    )
+
+    args = parser.parse_args()
+
+    LifeGame(
+        shape=(args.nrows, args.ncols),
+        symbol_type=args.symbols,
+        margin=args.margin,
+        fps=args.fps
+    ).run()
 
 
-def fgen():
-    loc = np.array([0, 0], dtype=np.int8)
-    for index, status in np.ndenumerate(cache):
-        count = 0
-        for rloc in rlocs:
-            if rloc == (0, 0):
-                continue
-            loc[0], loc[1] = index[0] + rloc[0], index[1] + rloc[1]
-            if loc[0] == cache.shape[0]:
-                loc[0] = 0
-            if loc[1] == cache.shape[1]:
-                loc[1] = 0
-            if cache[loc[0], loc[1]] == 1:
-                count += 1
-        if status == 0 and count == 3:
-            frame[index[0], index[1]] = 1
-        elif status == 1 and count not in {2, 3}:
-            frame[index[0], index[1]] = 0
-
-
-while True:
-    fprint(frame)
-    fgen()
-    cache[:] = frame
-    sleep(0.01)
-    os.system('clear')
+if __name__ == '__main__':
+    main()
